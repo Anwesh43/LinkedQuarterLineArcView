88eadd719759d16a0de3d722e79fe5e404a5d393
Anwesh43 <anweshthecool0@gmail.com>
2021-04-12 01:04:45 +0530

    adding QuarterLineArcView class

--- quartetlinearcview/src/main/java/com/example/quartetlinearcview/QuarterLineArcView.kt
+++ quartetlinearcview/src/main/java/com/example/quartetlinearcview/QuarterLineArcView.kt
@@ -56,3 +56,19 @@ fun Canvas.drawQLANode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawQuarterLineArc(scale, w, h, paint)
 }
+
+class QuarterLineArcView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
