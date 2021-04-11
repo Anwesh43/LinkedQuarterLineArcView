f5ff10232f7e8e46572ee1d7f79def1ceee56be8
Anwesh43 <anweshthecool0@gmail.com>
2021-04-12 01:22:13 +0530

    adding QLANode class

--- quartetlinearcview/src/main/java/com/example/quartetlinearcview/QuarterLineArcView.kt
+++ quartetlinearcview/src/main/java/com/example/quartetlinearcview/QuarterLineArcView.kt
@@ -119,4 +119,45 @@ class QuarterLineArcView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class QLANode(var i : Int, val state : State = State()) {
+
+        private var next : QLANode? = null
+        private var prev : QLANode?  = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = QLANode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawQLANode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : QLANode {
+            var curr : QLANode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
