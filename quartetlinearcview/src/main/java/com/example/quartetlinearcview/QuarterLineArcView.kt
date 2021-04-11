3aad5a38eb4448040cc506e64d308bbb0783b853
Anwesh43 <anweshthecool0@gmail.com>
2021-04-12 00:43:05 +0530

    adding constants and libraries

--- quartetlinearcview/src/main/java/com/example/quartetlinearcview/QuarterLineArcView.kt
+++ quartetlinearcview/src/main/java/com/example/quartetlinearcview/QuarterLineArcView.kt
@@ -1,2 +1,30 @@
 package com.example.quartetlinearcview
 
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+
+val colors : Array<Int> = arrayOf(
+    "#f44336",
+    "#2196F3",
+    "#006064",
+    "#BF360C",
+    "#1A237E"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 5
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.9f
+val rFactor : Float = 11.2f
+val delay : Long = 20
+val rot : Float = 90f
+val backColor : Int = Color.parseColor("#BDBDBD")
+
+
