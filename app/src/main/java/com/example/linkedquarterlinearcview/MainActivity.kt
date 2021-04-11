f0431a1bfb6b112cfabc1364e6e907c3d70c5b60
Anwesh43 <anweshthecool0@gmail.com>
2021-04-12 01:38:32 +0530

    adding fullscreen support

--- app/src/main/java/com/example/linkedquarterlinearcview/MainActivity.kt
+++ app/src/main/java/com/example/linkedquarterlinearcview/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.linkedquarterlinearcview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.quartetlinearcview.QuarterLineArcView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        QuarterLineArcView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
